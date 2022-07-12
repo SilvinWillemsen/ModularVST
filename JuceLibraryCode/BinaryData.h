aa9d2bf2a405d09b797b942d076781b04910b838
SilvinWillemsen <silvinwillemsen@gmail.com>
2022-07-12 11:49:05 +0200

    changed preset order

--- JuceLibraryCode/BinaryData.h
+++ JuceLibraryCode/BinaryData.h
@@ -8,23 +8,23 @@
 
 namespace BinaryData
 {
-    extern const char*   Marimba_xml;
-    const int            Marimba_xmlSize = 25132;
-
-    extern const char*   Timpani_xml;
-    const int            Timpani_xmlSize = 1036;
-
-    extern const char*   Cello_xml;
-    const int            Cello_xmlSize = 2160;
-
-    extern const char*   BanjoLele_xml;
-    const int            BanjoLele_xmlSize = 6636;
-
     extern const char*   Guitar_xml;
     const int            Guitar_xmlSize = 7715;
 
     extern const char*   Harp_xml;
     const int            Harp_xmlSize = 19991;
+
+    extern const char*   BanjoLele_xml;
+    const int            BanjoLele_xmlSize = 6636;
+
+    extern const char*   Timpani_xml;
+    const int            Timpani_xmlSize = 1036;
+
+    extern const char*   Marimba_xml;
+    const int            Marimba_xmlSize = 25132;
+
+    extern const char*   Cello_xml;
+    const int            Cello_xmlSize = 2160;
 
     extern const char*   EmptyInstrument_xml;
     const int            EmptyInstrument_xmlSize = 52;
